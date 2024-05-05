22d3851e2069511bed188c71d37bcfda01ab2cc9
pgprajwal <pgprajwal99@gmail.com>
2024-05-05 19:58:00 +0530

    Added method signatures to repository layer

--- src/main/java/com/crio/rentRead/repositoryServices/BookRepositoryService.java
+++ src/main/java/com/crio/rentRead/repositoryServices/BookRepositoryService.java
@@ -1,5 +1,22 @@
 package com.crio.rentRead.repositoryServices;
 
+import java.util.List;
+
+import com.crio.rentRead.dto.Book;
+import com.crio.rentRead.dto.User;
+
 public interface BookRepositoryService {
+    
+    Book createBook(String title, String author, String genre, String availabilityStatus);
+
+    User rentBook(int bookId, String email);
+
+    User returnBook(int bookId, String email);
+
+    Book updateBook(int bookId, String title, String author, String genre, String availabilityStatus);
+
+    List<Book> findAllBooks();
+
+    void deleteBook(int bookId);
     
 }
--- src/main/java/com/crio/rentRead/repositoryServices/UserRepositoryService.java
+++ src/main/java/com/crio/rentRead/repositoryServices/UserRepositoryService.java
@@ -1,5 +1,9 @@
 package com.crio.rentRead.repositoryServices;
 
+import com.crio.rentRead.dto.User;
+
 public interface UserRepositoryService {
+    
+    User registerUser(String firstName, String lastName, String email, String password, String role);
     
 }
